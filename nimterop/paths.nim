6900b69a32b03c197fc4715590b1a2e5488ac277
Timothee Cour <timothee.cour2@gmail.com>
2019-02-02 18:31:58 -0800

    _

--- nimterop/paths.nim
+++ nimterop/paths.nim
@@ -1,7 +1,10 @@
 import os
 
 proc nimteropRoot*(): string =
-  currentSourcePath.parentDir.parentDir
+  # currentSourcePath.parentDir.parentDir
+  # workaround BUG windows D20190202T182024
+  const dir = currentSourcePath
+  dir.parentDir.parentDir
 
 proc nimteropBuildDir*(): string =
   ## all nimterop generated files go under here (gitignored)
