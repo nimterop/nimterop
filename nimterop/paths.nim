e310df7a601203f94353c4b7a2c1c58e683c4395
Timothee Cour <timothee.cour2@gmail.com>
2019-02-02 18:35:35 -0800

    _

--- nimterop/paths.nim
+++ nimterop/paths.nim
@@ -4,6 +4,9 @@ proc nimteropRoot*(): string =
   # currentSourcePath.parentDir.parentDir
   # workaround BUG windows D20190202T182024
   const dir = currentSourcePath
+  const ret = dir.parentDir.parentDir
+  let ret2 = dir.parentDir.parentDir
+  echo ("nimteropRoot", dir, currentSourcePath, ret, ret2)
   dir.parentDir.parentDir
 
 proc nimteropBuildDir*(): string =
